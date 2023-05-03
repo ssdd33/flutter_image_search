ccdfb7b013438c78e1b4662fa4a356c4f5c8b957
ssdd33 <beeblu.0@gmail.com>
2023-05-03 18:12:17 +0900

    home_screen...

--- lib/ui/home_screen.dart
+++ lib/ui/home_screen.dart
@@ -15,7 +15,44 @@ class HomeScreen extends StatelessWidget {
         backgroundColor: Colors.white,
         elevation: 0,
       ),
-      body: Container(),
+      body: Column(children: [
+        Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: TextField(
+            decoration: InputDecoration(
+              border: const OutlineInputBorder(
+                borderRadius: BorderRadius.all(Radius.circular(10.0)),
+              ),
+              suffixIcon: IconButton(
+                onPressed: () {},
+                icon: const Icon(Icons.search),
+              ),
+            ),
+          ),
+        ),
+        Expanded(
+          child: GridView.builder(
+              padding: const EdgeInsets.all(16),
+              itemCount: 10,
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                crossAxisSpacing: 16,
+                mainAxisSpacing: 16,
+              ),
+              itemBuilder: (context, index) {
+                return Container(
+                  decoration: const BoxDecoration(
+                    borderRadius: BorderRadius.all(Radius.circular(16)),
+                    image: DecorationImage(
+                      fit: BoxFit.cover,
+                      image: NetworkImage(
+                          'https://www.cyso.co.kr/data/editor/2110/20211004124533_7098fe26daa694e9da3a00aeba11c554_j2lh.jpg'),
+                    ),
+                  ),
+                );
+              }),
+        ),
+      ]),
     );
   }
 }
