dc23c729383835242e8fdc2d2d35fadc0e36df19
ssdd33 <beeblu.0@gmail.com>
2023-06-03 14:36:19 +0900

    상태를 불변상태로 관리하기 freezed

--- lib/presentation/home/home_state.dart
+++ lib/presentation/home/home_state.dart
@@ -1,10 +1,12 @@
 import 'package:flutter_image_search/domain/model/photo.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+part 'home_state.freezed.dart';
+part 'home_state.g.dart';
 
-class HomeState {
-  final bool isLoading;
-  final List<Photo> photos;
-  HomeState(this.isLoading, this.photos);
-  HomeState copy({bool? isLoading, List<Photo>? photos}) {
-    return HomeState(isLoading ??= this.isLoading, photos ??= this.photos);
-  }
+@freezed
+class HomeState with _$HomeState {
+  factory HomeState(bool isLoading, List<Photo> photos) = _HomeState;
+
+  factory HomeState.fromJson(Map<String, dynamic> json) =>
+      _$HomeStateFromJson(json);
 }
