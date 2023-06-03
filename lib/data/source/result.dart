171c13404777e57bec8bfb9644e8bea393cf0f90
ssdd33 <beeblu.0@gmail.com>
2023-06-03 11:35:04 +0900

    에러처리를 위한 result class

---
+++ lib/data/source/result.dart
@@ -0,0 +1,15 @@
+abstract class Result<T> {
+  factory Result.success(T data) = Success;
+  factory Result.Error(String message) = Error;
+}
+
+class Success<T> implements Result<T> {
+  final T data;
+  Success(this.data);
+}
+
+class Error<T> implements Result<T> {
+  final String message;
+
+  Error(this.message);
+}
